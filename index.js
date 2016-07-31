0fdb7598f99776c440187a8fc8a5d80cb3b683ee
Jason Casares <jasonc@csdrafting.com>
2016-07-31 16:04:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code solution here
+class President {
+  constructor(name, politcalParty, yearsInOffice, homeState) {
+    this.name = name
+    this.politcalParty = politcalParty
+    this.yearsInOffice = yearsInOffice
+    this.homeState = homeState
+  }
+}
