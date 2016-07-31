2f4716a1088e9b1c00596c2eb4daed921145de22
Jason Casares <jasonc@csdrafting.com>
2016-07-31 16:09:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 class President {
-  constructor(name, politcalParty, yearsInOffice, homeState) {
-    this.name = name
-    this.politcalParty = politcalParty
-    this.yearsInOffice = yearsInOffice
-    this.homeState = homeState
+  constructor(name, politicalParty, yearsInOffice, homeState) {
+    this.name = name;
+    this.politicalParty = politicalParty;
+    this.yearsInOffice = yearsInOffice;
+    this.homeState = homeState;
+  }
+
+  veto() {
+    return "NO!";
+  }
+
+  passBill() {
+    return "You can do that!";
+  }
+
+  doCharity() {
+    return "I like to help people.";
+  }
+
+  conductPressInterview() {
+    return "I am proud to be an American."
+  }
+
+  sayHi() {
+    return "Hi, my name is " + this.name + ". I am from " + this.homeState + ". I represent the " + this.politicalParty + "s, and was in office " + this.yearsInOffice + "."
   }
 }
